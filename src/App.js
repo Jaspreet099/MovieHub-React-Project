362e1fe16d2a45f2c5eaf7c476ca5dd09d2af9d7
Jaspreet singh <jaspreetsinghsaini099@gmail.com>
2022-06-17 00:21:01 +0530

    API connect and its working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import { useEffect } from 'react';
 
+//4f239a68
+
+const API_URL = 'http://www.omdbapi.com?apikey=4f239a68'
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(()=> {
+        searchMovies('Superman')
+    }, []);
     return(
         <h1>This is app</h1>
     )
